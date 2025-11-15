a73349bb861e5f7f4dde1d5691bd7b1b792bcce6
hasbi23308 <hasbi23308@gmail.com>
2025-11-15 14:18:16 +0700

    ubah health

--- src/Pages/Healt/Healt.js
+++ src/Pages/Healt/Healt.js
@@ -7,30 +7,32 @@ function Healt() {
             <div className={style.textmain}>
 
 
-
-                <h1>TrackYour <br />
+                <div className={style.text}>
+                      <h1>TrackYour <br />
                     Wellness
                 </h1>
                 <p>Monitor your sleep, meals, and hydration<br />
                     for a healthier lifestyle
                 </p>
+                </div>
+              
             </div>
 
-            <div className={style.buttonbutton}>
+            <div className={style.buttons}>
 
-                <div className={style.buttonkiri}>
+                <div className={style.button}>
                    
                     <h3>Sleep <br /> Schedule</h3>
                     <p>Track your sleep <br />patterns</p>
                 </div>
 
-                <div className={style.buttontengah}>
+                <div className={style.button}>
                     
                     <h3>Meal plan</h3>
                     <p>Organize your <br />daily meals</p>
                 </div>
 
-                <div className={style.buttonkanan}>
+                <div className={style.button}>
                     <h3>Water Tarcker </h3>
                     <p>Logo your daily <br />water intake</p>
                 </div>
